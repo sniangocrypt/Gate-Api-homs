c1c2d97c6b06fd04a5fe2df80838d3a3ed8f2085
sniangocrypt <denis0007000700070007@gmail.com>
2024-10-24 20:54:31 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,81 @@
+#Получение цены токенов на Gate
+
+#Создайте программу, которая будет делать GET-запрос к API биржи Gate и получать ордербук для монет BTC, ETH и SOL. Используйте документацию API, чтобы разобраться, как именно отправлять запросы.
+
+#Требования:
+
+#	1.	Используйте библиотеку requests для выполнения HTTP-запросов.
+
+#	2.	Программа должна запрашивать ордербук для следующих пар: BTC/USDT, ETH/USDT, SOL/USDT.
+
+#	3.	Обработайте возможные ошибки, которые могут возникнуть при выполнении запроса. Например, если файл не найден, или есть проблема с подключением, выведите понятное сообщение.
+
+#	4.	Полученные данные должны содержать информацию о текущих ордерах на покупку и продажу.
+
+
+import requests
+
+try:
+    host = "https://api.gateio.ws"
+    prefix = "/api/v4"
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    url = '/spot/order_book'
+    query_param = 'currency_pair=BTC_USDT'
+    r = requests.request('GET', host + prefix + url + "?" + query_param, headers=headers)
+    data = r.json()
+    try:
+        print(f"Данные об ордерах на покупку БТЦ(цена,объем):  {data['asks']}")
+        print(f"Данные об ордерах на продажу БТЦ(цена,объем):  {data['bids']}")
+        print()
+        print()
+    except KeyError:
+        print(f"Проверте правильность запрашиваемого эллемента")
+        print()
+        print()
+except requests.exceptions.ConnectionError:
+    print(f"Ошибка соединения, проверте подключение или адрес запроса- {host}")
+
+# ЗАПРОС ДЛЯ ETH/USDT
+
+# ЗАПРОС ДЛЯ ETH/USDT
+
+try:
+    host = "https://api.gateio.ws"
+    prefix = "/api/v4"
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    url = '/spot/order_book'
+    query_param = 'currency_pair=ETH_USDT'
+    r = requests.request('GET', host + prefix + url + "?" + query_param, headers=headers)
+    data = r.json()
+    try:
+        print(f"Данные об ордерах на покупку ETH(цена,объем): {data['asks']}")
+        print(f"Данные об ордерах на продажу ETH(цена,объем): {data['bids']}")
+        print()
+        print()
+    except KeyError:
+        print(f"Проверте правильность запрашиваемого эллемента")
+except requests.exceptions.ConnectionError:
+    print(f"Ошибка соединения, проверте подключение или адрес запроса- {host}")
+
+# ЗАПРОС ДЛЯ SOL/USDT
+
+try:
+    host = "https://api.gateio.ws"
+    prefix = "/api/v4"
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+
+    url = '/spot/order_book'
+    query_param = 'currency_pair=SOL_USDT'
+    r = requests.request('GET', host + prefix + url + "?" + query_param, headers=headers)
+    data = r.json()
+    try:
+        print(f"Данные об ордерах на покупку SOL(цена,объем): {data['asks']}")
+        print(f"Данные об ордерах на продажу SOL(цена,объем): {data['bids']}")
+        print()
+        print()
+    except KeyError:
+        print(f"Проверте правильность запрашиваемого эллемента")
+except requests.exceptions.ConnectionError:
+    print(f"Ошибка соединения, проверте подключение или адрес запроса- {host}")
